3ab331799000d76a98b72433ad664cebfbaef31b
Davis Landry <davislandry1@gmail.com>
2018-09-22 14:36:56 -0600

    updated isStopWord

--- homework/landry_davis_hw2/driver.cpp
+++ homework/landry_davis_hw2/driver.cpp
@@ -22,50 +22,17 @@ struct wordItem
     int count;
 };
 
-/**
-* Function name: getStopWords
-* Purpose: read stop word list from file and store into vector
-* @param ignoreWordFile - filename (the file storing ignore/stop words)
-* @param _vecIgnoreWords - store ignore words from the file (pass by reference)
-* @return - none
-* Note: The number of words is fixed to 50
-*/
 void getStopWords(const char *ignoreWordFileName, vector<string> &_vecIgnoreWords);
 
-/**
-* Function name: isStopWord
-* Purpose: to see if a word is a stop word
-* @param word - a word (which you want to check if it is a stop word)
-* @param _vecIgnoreWords - the vector type of string storing ignore/stop words
-* @return - true (if word is a stop word), or false (otherwise)
-*/
 bool isStopWord(string word, vector<string> &_vecIgnoreWords);
 
-/**
-* Function name: getTotalNumberNonStopWords
-* Purpose: compute the total number of words saved in the words array (including repeated words)
-* @param list - an array of wordItems containing non-stopwords
-* @param length - the length of the words array
-* @return - the total number of words in the words array (including repeated words multiple times)
-*/
 int getTotalNumberNonStopWords(wordItem list[], int length);
 
-/**
-* Function name: arraySort
-* Purpose: sort an array of wordItems, decreasing, by their count fields
-* @param list - an array of wordItems to be sorted
-* @param length - the length of the words array
-*/
 void arraySort(wordItem list[], int length);
 
-/**
-* Function name: printTopN
-* Purpose: to print the top N high frequency words
-* @param wordItemList - a pointer that points to a *sorted* array of wordItems
-* @param topN - the number of top frequency words to print
-* @return none
-*/
 void printTopN(wordItem wordItemList[], int topN);
+
+void resizeArray(wordItem *list[]);
 
 const int STOPWORD_LIST_SIZE = 50;
 
@@ -75,6 +42,7 @@ const int INITIAL_ARRAY_SIZE = 100;
 int main(int argc, char *argv[])
 {
     vector<string> vecIgnoreWords(STOPWORD_LIST_SIZE);
+		wordItem list[INITIAL_ARRAY_SIZE];
 
     // verify we have the correct # of parameters, else throw error msg & return
     if (argc != 4)
@@ -85,39 +53,135 @@ int main(int argc, char *argv[])
         return 0;
     }
 
-    /* **********************************
-    1. Implement your code here.
+	 	int numWordsToProcess = stoi(argv[1]);
+		string fileToProcess = argv[2];
+    getStopWords(argv[3], vecIgnoreWords);
 
-    Read words from the file passed in on the command line, store and
-    count the words.
+		ifstream inFile;
+		string data, word;
 
-    2. Implement the six functions after the main() function separately.
-    ********************************** */
+		cout << "Opening File: " << fileToProcess << endl;
+		inFile.open(fileToProcess);
+
+		if (inFile.is_open())
+		{
+			cout << "Opened successfully!" << endl;
+			while(getline(inFile, data))
+			{
+				stringstream ss(data);
+				ss >> word;
+				if(!isStopWord(word, vecIgnoreWords))
+				{
+
+				}
+			}
+
+			inFile.close(); //close the file
+			cout << "Stop words succesfully read into _vecIgnoreWords" << endl;
+		}
+		else
+		{
+			cout << "File unsuccessfully opened!!" << endl;
+			return -1;
+		}
 
     return 0;
 }
 
+/**
+* Function name: getStopWords
+* Purpose: read stop word list from file and store into vector
+* @param ignoreWordFile - filename (the file storing ignore/stop words)
+* @param _vecIgnoreWords - store ignore words from the file (pass by reference)
+* @return - none
+* Note: The number of words is fixed to 50
+*/
 void getStopWords(const char *ignoreWordFileName, vector<string> &_vecIgnoreWords)
 {
+	ifstream inFile;
+	string data, word;
 
+	cout << "Opening File: " << *ignoreWordFileName << endl;
+	inFile.open(ignoreWordFileName);
+
+	if (inFile.is_open())
+	{
+		cout << "Opened successfully!" << endl;
+		while(getline(inFile, data))
+		{
+			stringstream ss(data);
+			ss >> word;
+			_vecIgnoreWords.push_back(word);
+		}
+
+		inFile.close(); //close the file
+		cout << "Stop words succesfully read into _vecIgnoreWords" << endl;
+	}
+	else
+	{
+		cout << "File unsuccessfully opened!!" << endl;
+		return;
+	}
+
+	return;
 }
 
+/**
+* Function name: isStopWord
+* Purpose: to see if a word is a stop word
+* @param word - a word (which you want to check if it is a stop word)
+* @param _vecIgnoreWords - the vector type of string storing ignore/stop words
+* @return - true (if word is a stop word), or false (otherwise)
+*/
 bool isStopWord(string word, vector<string> &_vecIgnoreWords)
 {
-    return true;
+	for(int i = 0; i < STOPWORD_LIST_SIZE; i ++)
+	{
+		if(word == _vecIgnoreWords.at(i))
+		{
+			return true;
+		}
+	}
+	return false;
 }
 
+/**
+* Function name: getTotalNumberNonStopWords
+* Purpose: compute the total number of words saved in the words array (including repeated words)
+* @param list - an array of wordItems containing non-stopwords
+* @param length - the length of the words array
+* @return - the total number of words in the words array (including repeated words multiple times)
+*/
 int getTotalNumberNonStopWords(wordItem list[], int length)
 {
     return 0;
 }
 
+/**
+* Function name: arraySort
+* Purpose: sort an array of wordItems, decreasing, by their count fields
+* @param list - an array of wordItems to be sorted
+* @param length - the length of the words array
+*/
 void arraySort(wordItem list[], int length)
 {
 
 }
 
+/**
+* Function name: printTopN
+* Purpose: to print the top N high frequency words
+* @param wordItemList - a pointer that points to a *sorted* array of wordItems
+* @param topN - the number of top frequency words to print
+* @return none
+*/
 void printTopN(wordItem wordItemList[], int topN)
 {
 
+}
+
+void resizeArray(wordItem *list[])
+{
+	worditem * newList = new wordItem[2 * sizeof(*list)/sizeof(*list[0])];
+	
 }
